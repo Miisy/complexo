57d03f468c52ecac6df41537e62ab21401f41614
Thamires Sales <thamisales@hotmail.com>
2024-03-27 12:47:59 -0300

    some new changes at main.js

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const form = document.getElementById('form-deposito');
 function validaNome(nomeCompleto)
 {
     const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.lenght = 2;
+    return nomeComoArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e) {
     let formValido = false;
     e.preventDefault();
-
+    
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroConta = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor');
     const mensagemSucesso = 'Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroConta.value}';
-
+    formValido = validaNome(nomeBeneficiario.value)
     if (formValido)
     {    
         alert(mensagemSucesso);
